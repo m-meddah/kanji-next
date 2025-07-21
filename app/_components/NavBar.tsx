c52c132e455a87879f4ca2e04fe9cbe8e2d3de7e
m-meddah <meddah.madjid@gmail.com>
2025-07-21 23:26:20 +0200

    remove _components folder

--- app/_components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-import NavMenu from "@/components/ui/menu-hover-effects";
-
-export const NavBar = () => {
-  return (
-    <div className="bgflex w-full h-screen justify-center items-center">
-      <NavMenu />
-    </div>
-  );
-};
